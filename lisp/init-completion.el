83626f7e6d2cb19ed63af694aa0287c144a6c4b3
irishmac473 <irishmac473@icloud.com>
2020-05-28 03:34:06 -0500

    Add smartparens package

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -2,7 +2,7 @@
 ;; ### Author: Caleb McCaffery <irishmac473@icloud.com>              ###
 ;; ### File: init-completion.el                                      ###
 ;; #####################################################################
-;; ### Provides the package and config for company-mode              ###
+;; ### Provides the packages and config for completion frameworks    ###
 ;; #####################################################################
 
 ;; Company is a pluggable text completion framework for Emacs.
@@ -13,5 +13,10 @@
   (progn (global-company-mode 1)
          (setq company-idle-delay 0.1)
          (setq company-show-numbers t)))
+
+(use-package smartparens
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook #'smartparens-mode))
 
 (provide 'init-completion)
